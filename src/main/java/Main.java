4ed6674d897b0f2cd5613d9d47fed1299c497842
Aleksandra Zec <sanjazec04@gmail.com>
2026-05-17 01:50:17 +0200

    writes results to file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,4 @@
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.FileInputStream;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.util.BitSet;
@@ -13,20 +10,33 @@ import static java.lang.Math.ceil;
 import static java.nio.file.Files.readAllBytes;
 
 public class Main {
+    public static PrintWriter writer;
+
     public static void main(String[] args) throws IOException {
+        writer = new PrintWriter("results/results.txt", "UTF-8");
+        writer.println("path, ogSize, compSize, totalTime, readTime, freqTime, constructTime, encodeTime");
         encodeBook("books/alices_adventures_in_wonderland.pdf");
         encodeBook("books/aliceintxt.txt");
+        encodeBook("books/aliceindjvu.djvu");
+        encodeBook("books/aliceinepub.epub");
+        encodeBook("books/aliceinmobi.mobi");
+
         encodeBook("books/the_brothers_karamazov.pdf");
         encodeBook("books/karamazovintxt.txt");
+        encodeBook("books/karamazovindjvu.djvu");
+        encodeBook("books/karamazovinepub.epub");
+        encodeBook("books/karamazovinmobi.mobi");
+        writer.close();
     }
 
     public static void encodeBook(String path) throws IOException {
+        long startTime = System.currentTimeMillis();
         byte[] book= loadFile(path);
 
         BitSet bitSet = BitSet.valueOf(book);
 //        System.out.println(bitSet.get(0,16));
 //        System.out.println(bitSet.size());
-
+        long endTimeReading = System.currentTimeMillis();
         Map<BitSet, Integer> frequencyMap = new HashMap<>();
         for (int i = 0; i < bitSet.length(); i+=16) {
             frequencyMap.put(bitSet.get(i,i+16), frequencyMap.getOrDefault(bitSet.get(i,i+16),0) + 1);
@@ -37,11 +47,14 @@ public class Main {
         for (BitSet bitSet1 : frequencyMap.keySet()){
             probFreqMap.put(bitSet1, Double.valueOf(frequencyMap.get(bitSet1))/(bitSet.length()/16));
         }
+        long endTimeFrequencyBuilding = System.currentTimeMillis();
 //        System.out.println(probFreqMap.size());
         HuffmanTree huffmanTree = new HuffmanTree(probFreqMap);
+        long endTimeHuffmanConstruction = System.currentTimeMillis();
         BitSet encodedBitSet = huffmanTree.Encode(bitSet);
 //        System.out.println(encodedBitSet);
         byte[] encodedByte = encodedBitSet.toByteArray();
+        long endTime = System.currentTimeMillis();
 //        System.out.println(encodedByte[0]);
         int NumBytes = encodedByte.length;
         System.out.println("Encoding of "+ path);
@@ -49,7 +62,13 @@ public class Main {
         int NumBytes2 = array.length;
         System.out.println("Original File Bytes: " + NumBytes2);
         System.out.println("Compressed Bytes: " + NumBytes);
+        System.out.println("Total time: "+(endTime-startTime));
+        System.out.println("Time to read document: "+(endTimeReading-startTime));
+        System.out.println("Time to calculate frequencies: "+(endTimeFrequencyBuilding-endTimeReading));
+        System.out.println("Time to construct huffman: "+(endTimeHuffmanConstruction-endTimeFrequencyBuilding));
+        System.out.println("Time to encode: "+(endTime-endTimeHuffmanConstruction));
         System.out.println();
+        writer.println(path+","+NumBytes2+","+NumBytes+","+(endTime-startTime)+","+(endTimeReading-startTime)+","+(endTimeFrequencyBuilding-endTimeReading)+","+(endTimeHuffmanConstruction-endTimeFrequencyBuilding)+","+(endTime-endTimeHuffmanConstruction));
     }
     public static byte[] loadFile(String sourcePath) throws IOException
     {
